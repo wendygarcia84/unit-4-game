95c09e3a9ef78bbbc9f0b5549e311c610dac6197
Wendy Garcia <wendy.fern84@gmail.com>
2019-09-01 08:52:00 -0700

    HTML Layout using divs, CSS style added to the players section. Base power, attack power, and health points assigned to each player

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// Create 4 characters with the following properties: name, health points, attack power, base attack power, counter attack power, and function property reduce HP
+
+// Display all 4 characters in a white bg
+  //ask the user to make a choice
+
+// User chooses a character, and it becomes "player"
+  // Create variables playerChosen = true; playerHP, attackPower, basePower and assign them to the current player's values
+
+  // The rest of the characters become "enemies"
+
+//Display 3 enemies on an area bellow, in a red background each
+  // Ask the user to choose an oponent
+    // chooseOponent function 
+        //Create variables oponentHP and counterAttackPower
+        // OponentChosen = true
+        //Display oponent in an area below in a black bg
+        // Enable attack button
+
+// ----------- ATTACK BUTTON ------------- //
+
+// Check if player and oponent are chosen, otherwise do nothing
+// Oponent loses oponentHP according to player's current attackPower
+// Player gains attackPower by adding basePower to it
+// ----Couter Attack ---//
+// Player loses playerHP according to enemie's counterAttackPower
+// Check if playerHP >= 0 you lose! restart all the game
+  // if oponent >= 0, 
+    // Hide oponent, 
+    // oponenChoosen = false, player choses oponent (chooseOponent)
+
+
+
