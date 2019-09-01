97fe9d4366edc0286f1bbc7aca6575a9bfcb11c6
Wendy Garcia <wendy.fern84@gmail.com>
2019-09-01 13:57:50 -0700

    added fight section and fight button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,44 +3,61 @@
 // Display all 4 characters in a white bg
   //ask the user to make a choice
 
-// User chooses a character, and it becomes "player"
-  // Create variables playerChosen = true; playerHP, attackPower, basePower and assign them to the current player's values
-
-  // The rest of the characters become "enemies"
 $(document).ready(function() {
   var enemiesVar;
   var oponentVar;
+
+  
+  var playerHP;
+  var playerBP;
+  var playerAP;
+
+  var oponentHP;
+  var counterAttackPower;
+
   var playerChosen = false;
   var oponentChosen = false;
 
   
   $( ".player" ).on("click", function () {
     if (!playerChosen) { 
+
+    // User chooses a character, and it becomes "player"
+  // Create variables playerChosen = true; playerHP, attackPower, basePower and assign them to the current player's values
+    playerHP = $(this).attr("health-points");
+    playerBP = $(this).attr("base-power");
+    playerAP = playerBP;
+
+    //-----------SANITY CHECK ---------------///
+    console.log(`HP: ${playerHP}, BP ${playerBP}, AP ${playerAP}`);
+
     //remove element from area, keeping its value in a variable
     enemiesVar = $(this).siblings().not("h3").detach();
+
+    // The rest of the characters become "enemies"
     $(enemiesVar).addClass("enemies");
+
     //Display 3 enemies on an area bellow, in a red background each
     $(".enemies-section").append(enemiesVar);
     playerChosen = true;
-    } else if (!oponentChosen) { 
+
+    } else if (playerChosen && !oponentChosen) { 
+      //create oponent
+      oponentHP = $(this).attr("health-points");
+      counterAttackPower = $(this).attr("counter-attack-power");
+
       //remove element from area, keeping its value in a variable
       oponentVar = $(this).detach();
       $(oponentVar).addClass("oponent");
-      //Display 3 enemies on an area bellow, in a red background each
+
+      //Display oponent in "oponent section" in a red bgc
       $(".oponent-section").append(oponentVar);
       oponentChosen = true;
-      console.log(oponentVar);
+      console.log(`oponent HP ${oponentHP}, CAP ${counterAttackPower}`);
+
     }
   });
 
-    // Ask the user to choose an oponent
-      // chooseOponent function 
-
-//-------THIS CODE DOESN'T WORK-----------//
-
-          //Create variables oponentHP and counterAttackPower
-          // OponentChosen = true
-          //Display oponent in an area below in a black bg
           // Enable attack button
 
   // ----------- ATTACK BUTTON ------------- //
